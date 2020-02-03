48c36043a5c27ad52106d74dffa2b674d0fbf79b
EC2 Default User <ec2-user@ip-172-31-40-86.us-east-2.compute.internal>
2020-02-03 11:51:04 +0000

    php_18

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,15 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     Route::get('news/delete', 'Admin\NewsController@delete');
+    
     //課題4
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
-
+Route::get('/', 'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
 
 //課題3
 // Route::group(['prefix' => 'admin'], function() {
